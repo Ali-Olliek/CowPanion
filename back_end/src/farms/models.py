6624c7e31cb247c70630e79692ac7e022afc6c5b
AliOlliek <olliek95@gmail.com>
2022-07-06 21:16:02 +0300

    BE - Created Farm Model

--- back_end/src/farms/models.py
+++ back_end/src/farms/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Farm (models.model):
+    name = models.CharField(max_length=255) # => we can default farm name to farmer's name
+    
+    #Foreign Key 
+    farmer_Id = models.IntegerField()
+
+    class Meta:
+        db_table = "Farms"
