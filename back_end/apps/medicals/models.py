d23ae7840f02bcd45c3035b1e1b126071c37ad19
AliOlliek <olliek95@gmail.com>
2022-07-06 22:33:51 +0300

    BE -Directory Update- Medical History to Medicals

--- back_end/apps/medicals/models.py
+++ back_end/apps/medicals/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Medical(models.model):
+class Medical(models.Model):
     procedure_description = models.CharField(max_length=255)
     medication = models.CharField(max_length=255)
     done_at = models.DateTimeField(auto_now_add=True)
