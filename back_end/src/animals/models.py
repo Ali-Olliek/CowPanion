c7b44ec31b707c673d43e58df49fcec063f3f18a
AliOlliek <olliek95@gmail.com>
2022-07-06 21:15:50 +0300

    BE - Created Animal Model

--- back_end/src/animals/models.py
+++ back_end/src/animals/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Animal(models.model):
+    name = models.CharField(max_length=55)
+    species = models.CharField(max_length=255)
+    breed = models.CharField(null=True, blank=True, max_length=255)
+    DOB = models.CharField(null=True, blank=True, max_length=255)
+    status = models.SmallIntegerField()
+
+    # Animal's Foreign Keys
+    farm_Id = models.IntegerField()
+    medical_history = models.IntegerField(null=True, blank=True) 
+    performance_profile = models.IntegerField(null=True, blank=True)
+    milk_profile = models.IntegerField(null=True, blank=True)
+    reminders = models.IntegerField(null=True, blank=True)
+
+    class Meta: 
+        db_table = "Animals" 
