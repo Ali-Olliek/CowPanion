9e6f423c2b8bf3456f3a960b73ba1d65caae14f5
AliOlliek <olliek95@gmail.com>
2022-07-06 21:16:23 +0300

    BE - Created Medical History Model

--- back_end/src/medicals/models.py
+++ back_end/src/medicals/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Medical(models.model):
+    procedure_description = models.CharField(max_length=255)
+    medication = models.CharField(max_length=255)
+    done_at = models.DateTimeField(auto_now_add=True)
+
+    # foreing key
+    animal_Id = models.IntegerField()
