aee3c2c4b26d3af6174a67ae5e9ac67ab6dc4462
AliOlliek <olliek95@gmail.com>
2022-07-06 20:52:38 +0300

    BE - Created User Model

--- back_end/src/users/models.py
+++ back_end/src/users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class User(models.model):
+    name = models.CharField(max_length=45)
+    email = models.CharField(max_length=45)
+    password = models.CharField(max_length=255)
+    DOB = models.CharField(max_length=30)
+    user_Type = models.SmallIntegerField()
+
+    # Change Default Naming of Tables
+    class Meta:
+        db_table = "Users"
