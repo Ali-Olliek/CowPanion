f4050b94c02028e08b6f008dad554ec2be68e505
AliOlliek <olliek95@gmail.com>
2022-07-06 22:16:29 +0300

    BE - Created Feed Composition App and Model

---
+++ back_end/src/compositions/models.py
@@ -0,0 +1,13 @@
+from re import L
+from django.db import models
+
+class Composition(models.model):
+    energy = models.DecimalField(max_digits=5, decimal_places=2)
+    protein_content = models.DecimalField(max_digits=5, decimal_places=2)
+    DM_content = models.DecimalField(max_digits=5, decimal_places=2)
+    CF_content = models.DecimalField(max_digits=5, decimal_places=2)
+    Minerals = models.CharField(max_length=255)
+    vitamins = models.CharField(max_length=255)
+    
+    class Meta:
+        db_table = "Feed_Compositions"
