caaf4a90eb57989783172e830db746f752bd3977
AliOlliek <olliek95@gmail.com>
2022-07-07 10:49:24 +0300

    BE - Applied Migrations

--- back_end/apps/users/models.py
+++ back_end/apps/users/models.py
@@ -6,7 +6,7 @@ class User(models.Model):
     email = models.CharField(max_length=45)
     password = models.CharField(max_length=255)
     DOB = models.CharField(max_length=30)
-    user_Type = models.SmallIntegerField()
+    user_Type = models.SmallIntegerField(default=2)
 
     # Change Default Naming of Tables
     class Meta:
