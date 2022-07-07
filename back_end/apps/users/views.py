6cbc2b597507a37dee71688c228fdd8a9d2c03b1
AliOlliek <olliek95@gmail.com>
2022-07-07 07:07:46 +0300

    BE - Created Sign Up Function for Users

--- back_end/apps/users/views.py
+++ back_end/apps/users/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+# Import necessary models
+from .models import User
+
+# Functions
+
+# Sign Up
+
+def sign_up(request):
+
+    if request.method == "POST":
+        name = (request.POST['name'])
+        email = (request.POST['email'])
+        password = hash(request.POST['password'])
+        DOB = (request.POST['DOB'])
+        user_type = (request.POST['user_type'])
+
+        user = User(name, email, password, DOB, user_type)
+        user.save()
+
+        return JsonResponse({
+            "Code": 200,
+            "Status": "success",
+            "message": "user registered"
+        })
+
+    return JsonResponse({
+        "Code": 500,
+        "Status": "Unsupported GET request"
+    })
