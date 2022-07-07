3bc2e87ac3f761436cc5ce5910e0600632e8a78e
AliOlliek <olliek95@gmail.com>
2022-07-07 15:34:59 +0300

    BE - Created and Tested Sign in with JWT

--- back_end/apps/users/views.py
+++ back_end/apps/users/views.py
@@ -1,9 +1,19 @@
+from django.contrib.auth import logout
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.hashers import check_password, make_password
+import jwt
 
 # Import necessary models
+
 from .models import User
+
+# Response Status Codes
+# 200 -- Request handled successfully
+# 201 -- Created
+# 208 -- Already Exists
+# 500 -- General Internal Error
 
 # Functions
 
@@ -13,20 +23,21 @@ def sign_up(request):
 
     if request.method == "POST":
         
-        user_check = User.objects.check(email__exact=request.POST['email'])
+        user_check = User.objects.get(email__exact=request.POST['email'])
         
         if user_check:
 
+            # Already Registered
             return JsonResponse({
                 "code": 208,
-                "status": "sser already registered",
+                "status": "user already registered",
             })
-
+            
         elif not user_check:
             data = request.POST
             name = (data['name'])
             email = (data['email'])
-            password = hash(data['password'])
+            password = make_password(data['password'])
             DOB = (data['DOB'])
             user_Type = (data.get('user_type', 2))
 
@@ -40,16 +51,21 @@ def sign_up(request):
             
             user.save()
 
+            # Success
             return JsonResponse({
                 "code": 201,
                 "status": "success",
                 "message": "user registered"
             })
 
+    # General Request Error
     return JsonResponse({
         "code": 500,
-        "Status": "Unsupported GET request"
+        "Status": "Request Error"
     })
+
+
+@csrf_exempt
 
 def sign_in(request):
     
@@ -58,15 +74,21 @@ def sign_in(request):
         auth_user = User.objects.get(email__exact=request.POST['email'])
         
         if auth_user:
-            
-            password = hash(request.POST['password'])
 
-            if password == auth_user.password:
+            password_valid = check_password(request.POST['password'], auth_user.password)
 
+            if password_valid:
+                
+                jwt_token = jwt.encode({'some': 'payload'}, "auth_user", algorithm="HS256")
+
+                print(jwt_token)
+                # Success
                 return JsonResponse({
                     "code": 200,
                     "status": "success",
-                    "User": auth_user,
+                    "user_name": auth_user.name,
+                    "user_type": auth_user.user_Type,
+                    "token": jwt_token,
                 })
 
             # Incorrect password
