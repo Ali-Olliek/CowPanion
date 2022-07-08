550aa0ee04a59d4c69c796c909cec81cd911cc00
AliOlliek <olliek95@gmail.com>
2022-07-08 12:21:13 +0300

    BE - Created Middleware for Users App

---
+++ back_end/apps/users/middleware/usersmiddleware.py
@@ -0,0 +1,39 @@
+# Users Middleware
+# Checks JWT Token
+# Checks user_Type
+
+import jwt
+
+def jwt_validator (token):
+
+    payload = jwt.decode(token, '18795', algorithms=['HS256'])
+
+    if payload: return payload
+    
+    return payload
+
+
+def user_type_authorizer(request):
+
+    if request.method == "POST":
+        data = request.POST
+        token = data['jwt_token']
+
+        if jwt_validator(token):
+
+            return True
+        
+        return False
+
+
+    elif request.method == "GET": 
+        data = request.GET
+        token = data['jwt_token']
+
+        if jwt_validator(token):
+
+            return True
+        
+        return False
+
+
