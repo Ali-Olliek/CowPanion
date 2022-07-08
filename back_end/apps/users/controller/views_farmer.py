4eefff0a852d8a34c1c8b40704dd643d26b970f0
AliOlliek <olliek95@gmail.com>
2022-07-08 18:24:00 +0300

    BE - Function Middleware Refactoring

--- back_end/apps/users/controller/views_farmer.py
+++ back_end/apps/users/controller/views_farmer.py
@@ -26,39 +26,47 @@ from ...animals.models import Animal
 
 
 # Functions 
+        # return JsonResponse({
+        #     "code": 401,
+        #     "status": "UNAUTH"
+        # })
 
 @csrf_exempt
 def create_farm(request):
 
-    if not user_authorizer(request):
+    user = user_authorizer(request)
+    user_type = user['user_type']
+
+    if user and user_type == 2:
+
+        if request.method == "POST":
+
+            data = request.POST
+
+            farm_name = data['name']
+            farmer = User.objects.get(id = data['user_id'])
+
+            farm = Farm (
+                name=farm_name,
+                farmer=farmer,
+            )
+            
+            farm.save()
+
+            return JsonResponse({
+                "code": 201,
+                "status": "Farm Created Successfully"
+            })
+        
         return JsonResponse({
+            "code": 500,
+            "status": "Check Request Method"
+        })
+        
+    return JsonResponse({
             "code": 401,
             "status": "UNAUTH"
         })
-
-    if request.method == "POST":
-
-        data = request.POST
-
-        farm_name = data['name']
-        farmer = User.objects.get(id = data['user_id'])
-
-        farm = Farm (
-            name=farm_name,
-            farmer=farmer,
-        )
-        
-        farm.save()
-
-        return JsonResponse({
-            "code": 201,
-            "status": "Farm Created Successfully"
-        })
-    
-    return JsonResponse({
-        "code": 500,
-        "status": "Check Request Method"
-    })
 
 
 def add_animal(request):
