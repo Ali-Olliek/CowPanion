30da2f59740b26b1d6af0ce49d9e2edfde0fabf3
AliOlliek <olliek95@gmail.com>
2022-07-11 12:31:02 +0300

    BE - Updated Milk Model to Be Related To Farm

--- back_end/apps/profiles/milks/models.py
+++ back_end/apps/profiles/milks/models.py
@@ -1,12 +1,13 @@
 from django.db import models
-from ...animals.models import Animal
+from ...farms.models import Farm
+
 
 class Milk(models.Model):
 
     quantity = models.IntegerField()
     
     # Milk Profile's foreign key
-    animal = models.ForeignKey(Animal, on_delete=models.CASCADE)
+    Farm = models.ForeignKey(Farm, on_delete=models.CASCADE)
 
     class Meta:
         db_table = "Milk_Profiles"
