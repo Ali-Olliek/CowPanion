e22661ee3c60d7746da45c5f723f2b5d76df18e8
AliOlliek <olliek95@gmail.com>
2022-07-12 18:40:10 +0300

    BE - Created Admin Controller

--- back_end/apps/users/controller/views_admin.py
+++ back_end/apps/users/controller/views_admin.py
@@ -1,5 +1,70 @@
 # Admin Actions
 
+from django.http import JsonResponse
+from ..middleware.usersmiddleware import user_type_authorizer
+
 # Necessary Models
 
+from ...users.models import User
+from ...farms.models import Farm
+from ...animals.models import Animal
+
 #Functions
+
+
+# Get all users in database (Farmers, and Vets)
+def get_users(request):
+
+    admin = user_type_authorizer(request)
+
+    if admin == 1:
+
+        if request.method == "POST":
+
+            users = User.objects.all()
+        
+            return JsonResponse({
+                "code": 200,
+                "status": "success",
+                "users": users,
+            })
+
+        return JsonResponse({
+            "code": 500,
+            "status": "USGE"
+        })
+
+    return JsonResponse({
+        "code": 401,
+        "status": "UNAUTH"
+    })
+
+# Get all animals (regardless of farm)
+def get_all_animals(request):
+
+    admin = user_type_authorizer(request)
+
+    if admin == 1:
+
+        if request.method == "GET":
+
+            animals = Animal.objects.all()
+
+            return JsonResponse({
+                "code": 200,
+                "status": "success",
+                "animals": animals
+            })
+
+        return JsonResponse({
+            "code": 500,
+            "status": "USGE"
+        })
+
+    return JsonResponse({
+        "code": 401,
+        "status": "UNAUTH"
+    })
+
+# Get all animals of a specific farm/farmer
+
