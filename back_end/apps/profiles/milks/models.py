813e687bb56e84b6c757356750d33af7eca8528e
AliOlliek <olliek95@gmail.com>
2022-07-12 17:33:51 +0300

    BE - Adjusted Milk Model and Migrations

--- back_end/apps/profiles/milks/models.py
+++ back_end/apps/profiles/milks/models.py
@@ -5,6 +5,7 @@ from ...farms.models import Farm
 class Milk(models.Model):
 
     quantity = models.IntegerField()
+    fat_content = models.IntegerField()
     
     # Milk Profile's foreign key
     Farm = models.ForeignKey(Farm, on_delete=models.CASCADE)
