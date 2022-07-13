0293c713f7660aea75da90a7026c68f2eba0bd2a
AliOlliek <olliek95@gmail.com>
2022-07-13 19:18:42 +0300

    BE - Added Function to Scrape PDF and Json Format it

--- back_end/utils/utility_functions.py
+++ back_end/utils/utility_functions.py
@@ -1,7 +1,8 @@
 import base64
-import camelot
 from io import BytesIO
-from PyPDF2 import PdfFileReader, PdfFileWriter
+import tabula
+import json 
+import csv
 
 # Function To Create B64 Format
 def get_base64(image):
@@ -10,4 +11,29 @@ def get_base64(image):
     img_str = base64.b64encode(buffered.getvalue())
     return "data:image/jpeg;base64," + img_str.decode()
 
+# turn csv to json
+def convert_to_json(csvfile, jsonfile):
 
+    data = []
+
+    with open(csvfile, encoding='ISO 8859-1') as csvf:
+        csvReader = csv.DictReader(csvf)
+
+        for rows in csvReader:
+            data.append(rows)
+    
+    with open(jsonfile, 'w', encoding='utf-8') as jsonf:
+        jsonf.write(json.dumps(data))
+
+# scrape pdf tables 
+def scrape_data():
+    file = "C:/Users/Ollie/Desktop/feeds.pdf"
+    tabula.read_pdf(file, pages=92)
+    tabula.convert_into(file, "utils/feeds.csv", pages="92-100", output_format="csv", stream=True)
+
+    csvfile = 'utils/feeds.csv'
+    jsonfile = 'utils/feeds.json'
+
+    convert_to_json(csvfile, jsonfile)
+
+scrape_data()
