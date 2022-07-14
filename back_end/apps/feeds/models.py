90c5e91fbbdbe782c9789ceb116a5edd98455c52
AliOlliek <olliek95@gmail.com>
2022-07-14 12:36:34 +0300

    BE - Adjusted Feed Model

--- back_end/apps/feeds/models.py
+++ back_end/apps/feeds/models.py
@@ -5,13 +5,23 @@ class Feed(models.Model):
     
     name = models.CharField(max_length=50)
     category = models.CharField(max_length=50, null=True, blank=True)
-    crude_protein_CP = models.FloatField(null=True, blank=True)
-    ether_extract_EE = models.FloatField(null=True, blank=True)
-    crude_fibre_CF = models.FloatField(null=True, blank=True)
-    nitrogen_free_extract_NFE = models.FloatField(null=True, blank=True)
-    neutral_detergent_fibre_NDF = models.FloatField(null=True, blank=True)
-    acid_detergent_fibre_ADF = models.FloatField(null=True, blank=True)
-    mineral_content_ASH = models.FloatField(null=True, blank=True)
+    crude_protein_CP = models.CharField(max_length=50,null=True, blank=True, default=0)
+    ether_extract_EE = models.CharField(
+        max_length=50, null=True, blank=True, default=0)
+    crude_fibre_CF = models.CharField(
+        max_length=50, null=True, blank=True, default=0)
+    nitrogen_free_extract_NFE = models.CharField(
+        max_length=50, null=True, blank=True, default=0)
+    neutral_detergent_fibre_NDF = models.CharField(
+        max_length=50, null=True, blank=True, default=0)
+    acid_detergent_fibre_ADF = models.CharField(
+        max_length=50, null=True, blank=True, default=0)
+    mineral_content_ASH = models.CharField(
+        max_length=50, null=True, blank=True, default=0)
+    Lignin = models.CharField(
+        max_length=50, null=True, blank=True, default=0)
+    Metabolizable_Energy_ME = models.CharField(
+        max_length=50, null=True, blank=True, default=0)
 
     class Meta:
         db_table = "Feeds"
