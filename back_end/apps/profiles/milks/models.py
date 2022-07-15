3537fe1dcad5cccc8c65561a8b27ebeef48037ac
AliOlliek <olliek95@gmail.com>
2022-07-15 21:51:11 +0300

    BE - Adjusted Fat Content To Be Null

--- back_end/apps/profiles/milks/models.py
+++ back_end/apps/profiles/milks/models.py
@@ -5,7 +5,7 @@ from ...farms.models import Farm
 class Milk(models.Model):
 
     quantity = models.IntegerField()
-    fat_content = models.IntegerField()
+    fat_content = models.IntegerField(null=True, blank=True)
     day = models.DateField(auto_now_add=True, blank=True, null=True)
     
     # Milk Profile's foreign key
