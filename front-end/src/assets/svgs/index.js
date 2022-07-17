6758ca27053c4dd0b4d92b218e14e9c0caf3eeb3
AliOlliek <olliek95@gmail.com>
2022-07-17 18:48:16 +0300

    FE - Created Assets

---
+++ front-end/src/assets/svgs/index.js
@@ -0,0 +1,5 @@
+import { HumiditySensor } from "./Humidity.svg";
+import { Thermostat } from "./Thermostat.svg";
+import { WindSock } from "./WindSock.svg";
+
+export default { HumiditySensor, Thermostat, WindSock };
