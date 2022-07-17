0a0a837b0675d25245e3f58c4b5243bfc8767f21
AliOlliek <olliek95@gmail.com>
2022-07-17 10:45:46 +0300

    FE - Created Index file for Atomic Components for Exports

---
+++ front-end/src/components/UI/atoms/index.js
@@ -0,0 +1,13 @@
+import * as CameraButton from "./CameraButton";
+import * as CowsListButton from "./CowsListButton";
+import * as HomeButton from "./HomeButton";
+import * as ProfileButton from "./ProfileButton";
+import * as TodoListButton from "./TodoListButton";
+
+export {
+  CameraButton,
+  CowsListButton,
+  HomeButton,
+  ProfileButton,
+  TodoListButton,
+};
