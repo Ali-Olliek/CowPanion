0b6f92c1aff6b01326279d60fdbd5d1e42dace4f
AliOlliek <olliek95@gmail.com>
2022-07-17 16:07:29 +0300

    FE - Created Weather Card Atomic Component

---
+++ front-end/src/components/UI/atoms/WeatherCard.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+export function WeatherCard() {
+  return (
+    <View>
+      <Text>Reading</Text>
+      <Text>Unit</Text>
+      <Text>Icon</Text>
+    </View>
+  );
+}
