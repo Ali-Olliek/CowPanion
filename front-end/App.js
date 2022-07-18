7fa2d7ae1f8865bc6053d422afa5bff6bbe89910
AliOlliek <olliek95@gmail.com>
2022-07-18 20:11:32 +0300

    FE - Misc Updates and Minor Fixing

--- front-end/App.js
+++ front-end/App.js
@@ -2,13 +2,20 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { LandingPage } from "./src/components/pages/LandingPage";
 import { SignUpPage } from "./src/components/pages/SignUpPage";
+import { AttributeBoxes } from "./src/components/UI/molecules/AttributeBoxes";
+import { AnimalRecord } from "./src/components/UI/molecules/AnimalRecord";
+import { AnimalsListPage } from "./src/components/pages/AnimalsListPage";
 
 export default function App() {
   return (
     <View>
       <StatusBar style="auto" />
-      <SignUpPage />
+      {/* <SignUpPage /> */}
       {/* <LoginPage /> */}
+      {/* <LandingPage /> */}
+      {/* <AttributeBoxes /> */}
+      {/* <AnimalRecord /> */}
+      <AnimalsListPage />
     </View>
   );
 }
--- front-end/src/styles/index.js
+++ front-end/src/styles/index.js
@@ -1,17 +1,9 @@
 // Import the files within the folder and export them as one package
 
 import * as Buttons from "./buttons";
-import * as Colors from "./colors";
 import * as Spacing from "./spacing";
 import * as Typography from "./typography";
 import * as bottomNavBarStyle from "./bottomNavBarStyle";
 import * as LandingPageStyle from "./landingPageStyle";
 
-export {
-  Buttons,
-  Colors,
-  Spacing,
-  Typography,
-  bottomNavBarStyle,
-  LandingPageStyle,
-};
+export { Buttons, Spacing, Typography, bottomNavBarStyle, LandingPageStyle };
