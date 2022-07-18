28b6a31ba2f89618ecaf69f1f1b3d4948d06ba2f
AliOlliek <olliek95@gmail.com>
2022-07-18 17:41:21 +0300

    FE - Deleted Unwanted Assets

--- front-end/src/assets/svgs/index.js
+++
@@ -1,5 +0,0 @@
-import { HumiditySensor } from "./Humidity.svg";
-import { Thermostat } from "./Thermostat.svg";
-import { WindSock } from "./WindSock.svg";
-
-export default { HumiditySensor, Thermostat, WindSock };
