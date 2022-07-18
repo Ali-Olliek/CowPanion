f23d5b7c19d364812a60cfb31fe347ca875615fd
AliOlliek <olliek95@gmail.com>
2022-07-18 13:19:48 +0300

    FE - Refactored Component Rendering

--- front-end/src/components/UI/atoms/GeneralStatsCard.js
+++ front-end/src/components/UI/atoms/GeneralStatsCard.js
@@ -2,21 +2,20 @@ import { View, Text } from "react-native";
 import { stats } from "../../../styles/statsStyle";
 
 export function GeneralStatsCard() {
-  const titles = ["Cows", "Milking Cows", "Milk", "Dry Cows"];
-  const stat = ["30", "12", "400", "4"];
+  const statistics = {
+    cows: 30,
+    "Milking Cows": 12,
+    "Milk Production": 500,
+    "Dry Cows": 4,
+  };
 
-  return (
-    <View style={stats.container}>
-      {titles.map((title, index) => {
-        return (
-          <View style={stats.card}>
-            <Text key={index} style={stats.stat}>
-              {title}
-            </Text>
-            <Text style={stats.title}>test</Text>
-          </View>
-        );
-      })}
-    </View>
-  );
+  const displayStatistics = Object.entries(statistics).map(([title, value]) => {
+    return (
+      <View style={stats.card}>
+        <Text style={stats.stat}>{title}</Text>
+        <Text style={stats.stat}>{value}</Text>
+      </View>
+    );
+  });
+  return <View style={stats.container}>{displayStatistics}</View>;
 }
--- front-end/src/components/UI/molecules/GeneralStatsList.js
+++ front-end/src/components/UI/molecules/GeneralStatsList.js
@@ -1,7 +1,1 @@
-import { View, Text } from "react-native";
-import React from "react";
-import { GeneralStatsCard } from "../atoms/GeneralStatsCard";
 
-export function GeneralStatsList() {
-  return <GeneralStatsCard />;
-}
