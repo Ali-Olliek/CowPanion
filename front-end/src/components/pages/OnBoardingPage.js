2672f0a97a6f4115f65988712a6cefffdb22684d
AliOlliek <olliek95@gmail.com>
2022-07-22 13:16:09 +0300

    FE - Created OnBoarding UI

---
+++ front-end/src/components/pages/OnBoardingPage.js
@@ -0,0 +1,56 @@
+import Onboarding from "react-native-onboarding-swiper";
+import { Image } from "react-native";
+import { onBoardingStyle } from "../../styles/onBoardingStyle";
+export function OnBoardingPage({ navigation }) {
+  return (
+    <Onboarding
+      onDone={() => navigation.navigate("LogIn")}
+      pages={[
+        {
+          backgroundColor: "#fff",
+          image: (
+            <Image
+              style={onBoardingStyle.image}
+              source={require("../../assets/images/Logo.png")}
+            />
+          ),
+          title: "CowPanion",
+          subtitle: "Manage your farm with ease",
+        },
+        {
+          backgroundColor: "#fff",
+          image: (
+            <Image
+              style={onBoardingStyle.image}
+              source={require("../../assets/images/chores.png")}
+            />
+          ),
+          title: "Tasks Management",
+          subtitle: "Create and manage your farm related tasks",
+        },
+        {
+          backgroundColor: "#fff",
+          image: (
+            <Image
+              style={onBoardingStyle.image}
+              source={require("../../assets/images/analysis.png")}
+            />
+          ),
+          title: "Analyze",
+          subtitle: "Visualize your farm's performance",
+        },
+        {
+          backgroundColor: "#fff",
+          image: (
+            <Image
+              style={onBoardingStyle.image}
+              source={require("../../assets/images/Link.png")}
+            />
+          ),
+          title: "Link with your Veterinarian",
+          subtitle: "Veterinarians can check up on your animal's status",
+        },
+      ]}
+    />
+  );
+}
---
+++ front-end/src/styles/onBoardingStyle.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Dimensions } from "react-native";
+
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
+
+const onBoardingStyle = StyleSheet.create({
+  image: {
+    height: 220,
+    width: 220,
+  },
+});
+
+export { onBoardingStyle };
