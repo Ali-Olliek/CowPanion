8e2134f510c6bbed946d57b9aff789bd864cbd1d
AliOlliek <olliek95@gmail.com>
2022-07-22 07:23:18 +0300

    FE - Created Bottom Nav Bar

--- front-end/src/components/UI/organisms/BottomNavBarButtons.js
+++ front-end/src/components/UI/organisms/BottomNavBarButtons.js
@@ -1,23 +1,67 @@
 // React
-import { View } from "react-native";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
+// Pages
 import {
-  CameraButton,
-  CowsListButton,
-  HomeButton,
-  ProfileButton,
-  TodoListButton,
-} from "../atoms";
-
-import { bottomNavBarStyle } from "front-end/src/styles/bottomNavBarStyle.js";
+  AnimalsListPage,
+  CreateFarmPage,
+  RemindersPage,
+  ProfilePage,
+  MainPage,
+} from "../../pages";
 
 export function BottomNavBarButtons() {
+  const Tab = createMaterialBottomTabNavigator();
   return (
-    <View style={bottomNavBarStyle.container}>
-      <HomeButton />
-      <CowsListButton />
-      <CameraButton />
-      <TodoListButton />
-      <ProfileButton />
-    </View>
+    <Tab.Navigator
+      screenOptions={{ headerShown: false }}
+      initialRouteName="Home"
+      activeColor="#D46C4E"
+      inactiveColor="#000000"
+      barStyle={{ backgroundColor: "#ACC0B3" }}
+    >
+      <Tab.Screen
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={26} />
+          ),
+        }}
+        name={"Home"}
+        component={MainPage}
+      />
+      <Tab.Screen
+        options={{
+          tabBarLabel: "Animals",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="cow" size={26} color={color} />
+          ),
+        }}
+        name={"AnimalsList"}
+        component={AnimalsListPage}
+      />
+      <Tab.Screen
+        options={{
+          tabBarLabel: "Reminders",
+          tabBarIcon: ({ color }) => (
+            <Feather name="list" size={24} color={color} />
+          ),
+        }}
+        name={"Reminders"}
+        component={RemindersPage}
+      />
+      <Tab.Screen
+        options={{
+          tabBarLabel: "Reminders",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="person" size={24} color={color} />
+          ),
+        }}
+        name={"Profile"}
+        component={ProfilePage}
+      />
+    </Tab.Navigator>
   );
 }
