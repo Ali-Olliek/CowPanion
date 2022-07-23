9460f020cd947ea3029b3390052d9798c866e406
AliOlliek <olliek95@gmail.com>
2022-07-23 11:00:00 +0300

    FE - Wrapped the App with Store Provider

--- front-end/App.js
+++ front-end/App.js
@@ -1,22 +1,30 @@
 import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Provider } from "react-redux";
+import { OnBoardingPage } from "./src/components/pages/OnBoardingPage";
+import { configureStore } from "@reduxjs/toolkit";
+
 import {
   AnimalInformationPage,
   AnimalsListPage,
-  CreateFarmPage,
   CreateMedicalRecordPage,
   CreateReminderPage,
   LandingPage,
   LoginPage,
   SignUpPage,
   RemindersPage,
+  UpdateStatusPage,
+  ProfilePage,
 } from "./src/components/pages";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import { store } from "./src/redux/store";
 
 export default function App() {
   const Stack = createNativeStackNavigator();
+
   return (
-    <>
+    <Provider store={store}>
       <NavigationContainer>
         <Stack.Navigator
           initialRouteName="SignIn"
@@ -29,9 +37,16 @@ export default function App() {
           <Stack.Screen name="Animals" component={AnimalsListPage} />
           <Stack.Screen name="Animal" component={AnimalInformationPage} />
           <Stack.Screen name="Reminders" component={RemindersPage} />
+          <Stack.Screen name="OnBoarding" component={OnBoardingPage} />
+          <Stack.Screen
+            name="New Medical Record"
+            component={CreateMedicalRecordPage}
+          />
+          <Stack.Screen name="userProfile" component={ProfilePage} />
+          <Stack.Screen name="Update Status" component={UpdateStatusPage} />
         </Stack.Navigator>
       </NavigationContainer>
-    </>
+    </Provider>
   );
 }
 
