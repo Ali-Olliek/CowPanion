9fd23b2e4ac69434fa952fcbb5b83a6e7e78e9d5
AliOlliek <olliek95@gmail.com>
2022-07-23 17:22:05 +0300

    BE - Adjusted User Model To Set DOB To Null

--- back_end/apps/users/models.py
+++ back_end/apps/users/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
+
 class User(models.Model):
-    
+
     name = models.CharField(max_length=45)
     email = models.CharField(max_length=45, unique=True)
     password = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=100, null=True, blank=True)
-    DOB = models.CharField(max_length=30)
+    DOB = models.CharField(max_length=30, null=True, blank=True)
     user_Type = models.SmallIntegerField(default=2)
     is_logged = models.BooleanField(default=False)
 
