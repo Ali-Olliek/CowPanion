076acd1b1379ec60438b80b90ea4d040ec367b08
AliOlliek <olliek95@gmail.com>
2022-07-24 16:41:51 +0300

    FE - Linked Animals List Page To Django Server

--- front-end/src/components/UI/molecules/AnimalRecord.js
+++ front-end/src/components/UI/molecules/AnimalRecord.js
@@ -1,24 +1,34 @@
-import { View, Text, FlatList, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  SafeAreaView,
+} from "react-native";
 import { styles } from "../../../styles/AnimalsListStyle";
 
 export function AnimalRecord({ animals, navigation }) {
   return (
-    <FlatList
-      data={Object.keys(animals)}
-      renderItem={({ item }) => (
-        <>
-          <TouchableOpacity
-            style={styles.recordContainer}
-            onPress={() => navigation.navigate("Animal")}
-          >
-            <Text style={styles.status}>{animals[item].Status}</Text>
-            <Text style={styles.animalAttr}>{animals[item].ID}</Text>
-            <Text style={styles.animalAttr}>{animals[item].Name}</Text>
-            <Text style={styles.animalAttr}>{animals[item].Age}</Text>
-          </TouchableOpacity>
-          <View style={styles.hr}></View>
-        </>
-      )}
-    />
+    <View>
+      <SafeAreaView>
+        <FlatList
+          data={Object.keys(animals)}
+          renderItem={({ item }) => (
+            <>
+              <TouchableOpacity
+                style={styles.recordContainer}
+                onPress={() => navigation.navigate("Animal")}
+              >
+                <Text style={styles.status}>{animals[item].Status}</Text>
+                <Text style={styles.animalAttr}>{animals[item].ID}</Text>
+                <Text style={styles.animalAttr}>{animals[item].Name}</Text>
+                <Text style={styles.animalAttr}>{animals[item].Age}</Text>
+              </TouchableOpacity>
+              <View style={styles.hr}></View>
+            </>
+          )}
+        />
+      </SafeAreaView>
+    </View>
   );
 }
--- front-end/src/components/pages/AnimalsListPage.js
+++ front-end/src/components/pages/AnimalsListPage.js
@@ -1,40 +1,69 @@
-// React
-import { View } from "react-native";
-
+// Modules
+import axios from "axios";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { View, Text, TouchableHighlight } from "react-native";
 // Styles
 import { styles } from "../../styles/AnimalsListStyle";
-
 // Components
 import { MainHeaderTitle } from "../UI/atoms";
+import { FixingError } from "../UI/atoms";
 import { AnimalRecord, AttributeBoxes } from "../UI/molecules";
 
 export function AnimalsListPage({ navigation }) {
-  const animals = [
-    {
-      Status: "PR",
-      ID: "6",
-      Name: "Betsy",
-      Age: "4",
-    },
-    {
-      Status: "D",
-      ID: "2",
-      Name: "Kiky",
-      Age: "2",
-    },
-  ];
+  const { token, id } = useSelector((state) => state.user.user);
+  const [animals, setAnimals] = useState([]);
+  const [fetchError, setFetchError] = useState(false);
 
+  // Constructing Request
+  const animalsListUrl = `http://10.0.2.2:8000/api/v1/animals/?user_id=${id}`;
+  const getAnimals = () => {
+    axios({
+      method: "GET",
+      url: animalsListUrl,
+      data: { user_id: id },
+      headers: {
+        Authorization: token,
+        "content-type": "multipart/form-data",
+      },
+    }).then((response) => {
+      if (response.data.code === 200) {
+        setAnimals(response.data.animals);
+      } else {
+        setFetchError(true);
+        setTimeout(() => {
+          setFetchError(false);
+        }, 1500);
+      }
+    });
+  };
+  getAnimals();
   return (
-    <View>
-      <View style={styles.header}>
-        <MainHeaderTitle title={"Animals List"} />
+    <>
+      <View>
+        <View style={styles.header}>
+          <MainHeaderTitle title={"Animals List"} />
+        </View>
+        <View style={styles.titles}>
+          <AttributeBoxes />
+        </View>
+        {fetchError ? <FixingError /> : null}
+        <View style={styles.list}>
+          {animals.length === 0 && !fetchError ? (
+            <View style={styles.messageContainer}>
+              <Text style={styles.messagePrimary}>No Animals</Text>
+              <Text style={styles.messageSecondary}>
+                Press The Orange Button To Add Animals To Your Farm
+              </Text>
+            </View>
+          ) : (
+            <AnimalRecord navigation={navigation} animals={animals} />
+          )}
+        </View>
       </View>
-      <View style={styles.titles}>
-        <AttributeBoxes />
+      <View style={styles.AddAnimal}>
+        <Text>+</Text>
       </View>
-      <View style={styles.list}>
-        <AnimalRecord navigation={navigation} animals={animals} />
-      </View>
-    </View>
+    </>
   );
 }
