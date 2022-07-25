54bd6e9058cb1e9c630d2468a6275e0db532c957
AliOlliek <olliek95@gmail.com>
2022-07-25 11:42:22 +0300

    FE - Adjusted Navigation in Animal Actions

--- front-end/src/components/UI/molecules/AnimalActions.js
+++ front-end/src/components/UI/molecules/AnimalActions.js
@@ -1,15 +1,18 @@
 import { View, Text, TouchableHighlight } from "react-native";
 import { infoStyles } from "../../../styles/AnimalInfoStyle";
 
-export function AnimalActions({ navigation }) {
+export function AnimalActions({ navigation, id }) {
   const actions = ["Update Status", "New Medical Record"];
-
   return (
     <View style={infoStyles.actionsContainer}>
       {actions.map((action, index) => {
         return (
           <TouchableHighlight
-            onPress={() => navigation.navigate(action)}
+            onPress={() =>
+              navigation.navigate(action, {
+                id: id,
+              })
+            }
             underlayColor={"white"}
           >
             <View key={index} style={infoStyles.actionCard}>
