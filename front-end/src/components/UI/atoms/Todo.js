bbfc0e85105c0935baa1c088c3acd0e6ad945259
AliOlliek <olliek95@gmail.com>
2022-07-25 16:31:26 +0300

    FE - Added Function to Delete Reminders

--- front-end/src/components/UI/atoms/Todo.js
+++ front-end/src/components/UI/atoms/Todo.js
@@ -1,15 +1,57 @@
+//
+// Modules
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 import { View, Text, TouchableWithoutFeedback } from "react-native";
+//
+// Styles
 import { Todos } from "../../../styles/TodosStyle";
 
-export function Todo({ description, due }) {
+export function Todo({ props, index }) {
+  //
+  // States and variables
+  const { token } = useSelector((state) => state.user.user);
+  const [id, setId] = useState(null);
+
+  //
+  // Calculate time left in days
+
+  let dueDate = new Date(props.fields.due_time);
+  let today = Date.now();
+  const diffTime = Math.abs(dueDate - today);
+  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+  //
+  // create Request
+  const deleteReminderUrl = `http://10.0.2.2:8000/api/v1/deleteReminder/?reminder_id=${id}`;
+  const data = { reminder_id: id };
+  const deleteReminder = () => {
+    axios({
+      url: deleteReminderUrl,
+      method: "GET",
+      headers: { Authorization: token },
+      data: data,
+    }).then((response) => {
+      console.log(response.data);
+    });
+  };
+  // when Id changes
+  useEffect(() => {
+    deleteReminder();
+  }, [id]);
+
   return (
     <View style={Todos.TodoContainer}>
       <View style={Todos.TodoInfo}>
-        <Text style={Todos.TodoDesc}>{description}</Text>
-        <Text style={Todos.TodoDue}>{due}</Text>
+        <Text style={Todos.TodoDesc}>{props.fields.task_title}</Text>
+        <Text style={Todos.TodoDue}>Due in: {diffDays} days</Text>
       </View>
       <View style={Todos.TodoAction}>
-        <TouchableWithoutFeedback>
+        <TouchableWithoutFeedback
+          onPress={() => {
+            setId(props.pk);
+          }}
+        >
           <Text style={Todos.Check}>✓</Text>
         </TouchableWithoutFeedback>
       </View>
