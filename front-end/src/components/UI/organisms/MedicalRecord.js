92ad86c0a215ae3a88c56606c68956c1302f346e
AliOlliek <olliek95@gmail.com>
2022-07-25 13:30:04 +0300

    FE - Improved UI of Medical Records

--- front-end/src/components/UI/organisms/MedicalRecord.js
+++ front-end/src/components/UI/organisms/MedicalRecord.js
@@ -1,7 +1,19 @@
-import { View, Text, FlatList } from "react-native";
-import React from "react";
+import { View, Text, FlatList, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
 import { infoStyles } from "../../../styles/AnimalInfoStyle";
 export function MedicalRecord({ records }) {
+  //
+  // States and Variables
+  const [clicked, setClicked] = useState("");
+  const dates = [];
+  for (let record in records) {
+    let date = new Date(records[record].fields.done_at);
+    let formattedDate = date.toLocaleDateString();
+    dates.push(formattedDate);
+  }
+
+  //
+  // Main
   return (
     <>
       {records.length === 0 ? (
@@ -13,12 +25,29 @@ export function MedicalRecord({ records }) {
           <Text>Medical Records</Text>
           <FlatList
             data={Object.keys(records)}
-            renderItem={({ item }) => (
-              <View style={infoStyles.card}>
-                <Text>{item.condition}</Text>
-                <Text>{item.medication}</Text>
-                <Text>{item.dateDone}</Text>
-              </View>
+            renderItem={({ item, index }) => (
+              <TouchableOpacity
+                onPress={() => setClicked(toString(index))}
+                style={[!clicked ? infoStyles.card : infoStyles.enlarge]}
+              >
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    width: 200,
+                  }}
+                >
+                  <Text>{records[item].fields.procedure_description}</Text>
+                  <Text>{dates[index]}</Text>
+                </View>
+                {clicked ? (
+                  <View style={{ width: 200 }}>
+                    <Text style={{ justifyContent: "flex-start" }}>
+                      {records[item].fields.medication}
+                    </Text>
+                  </View>
+                ) : null}
+              </TouchableOpacity>
             )}
           />
         </View>
