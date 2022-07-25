ed1a048e0d00623d16ec879382b2b2e799825d2d
AliOlliek <olliek95@gmail.com>
2022-07-25 21:24:12 +0300

    Fe - Linked Create Farm to Django BE

--- front-end/src/components/pages/CreateFarmPage.js
+++ front-end/src/components/pages/CreateFarmPage.js
@@ -1,29 +1,74 @@
+// Modules
+import axios from "axios";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
 import { View, Text, TextInput, TouchableHighlight } from "react-native";
-import React from "react";
-import { MainHeaderTitle } from "../UI/atoms";
+
+// Styles
 import { styles } from "../../styles/AnimalsListStyle";
 import { createFarmStyle } from "../../styles/createFarmStyle";
 
+//Components
+import { Map } from "../UI/organisms";
+import { MainHeaderTitle } from "../UI/atoms";
 // Generate Password For Farm and Send it with the post request
 
 export function CreateFarmPage() {
+  //
+  // States and variables
+  const [name, setName] = useState("");
+  const [container, setContainer] = useState("");
+  const [coordinate, setCoordinate] = useState();
+  const { id, token } = useSelector((state) => state.user.user);
+  const farm_password = Math.floor(Math.random() * 100);
+  //
+  // Create Request
+  const createFarmUrl = "http://10.0.2.2:8000/api/v1/createFarm/";
+  const data = {
+    name: name,
+    location: coordinate,
+    milk_container_volume: container,
+    farm_password: farm_password,
+    user_id: id,
+  };
+  const createFarm = () => {
+    axios({
+      method: "POST",
+      url: createFarmUrl,
+      data: data,
+      headers: { Authorization: token, "content-type": "multipart/form-data" },
+    })
+      .then((response) => {
+        if (response.data.code === 201) {
+          console.log("created farm");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <>
       <View style={styles.header}>
         <MainHeaderTitle title={"Create Farm"} />
       </View>
       <View style={createFarmStyle.midSec}>
-        <TextInput
-          style={createFarmStyle.inputs}
-          placeholder="Farm Name"
-          placeholderTextColor={"grey"}
-        ></TextInput>
-        <TextInput
-          style={createFarmStyle.inputs}
-          placeholder="Milk Container Volume (in cm3)"
-          placeholderTextColor={"grey"}
-        ></TextInput>
-        <TouchableHighlight>
+        <View style={createFarmStyle.inputsContainer}>
+          <TextInput
+            onChangeText={(farmName) => setName(farmName)}
+            style={createFarmStyle.inputs}
+            placeholder="Farm Name"
+            placeholderTextColor={"grey"}
+          ></TextInput>
+          <TextInput
+            onChangeText={(milkContainer) => setContainer(milkContainer)}
+            style={createFarmStyle.inputs}
+            placeholder="Milk Container Volume (in cm3)"
+            placeholderTextColor={"grey"}
+          ></TextInput>
+        </View>
+        <Map setCoordinate={setCoordinate} />
+        <TouchableHighlight onPress={createFarm}>
           <View style={createFarmStyle.GO}>
             <Text>GO</Text>
           </View>
