f614e4d72976b20d678cdc18ff2a4c2535fe1b7c
AliOlliek <olliek95@gmail.com>
2022-07-25 17:43:18 +0300

    FE - Displayed Name in Welcome Message

--- front-end/src/components/UI/atoms/HeaderTitle.js
+++ front-end/src/components/UI/atoms/HeaderTitle.js
@@ -1,10 +1,11 @@
 import { View, Text } from "react-native";
 import { headerSectionStyle } from "../../../styles/headerSectionStyle";
-
+import { useSelector } from "react-redux";
 export function HeaderTitle() {
+  const { name } = useSelector((state) => state.user.user);
   return (
     <View>
-      <Text style={headerSectionStyle.title}>Welcome, User</Text>
+      <Text style={headerSectionStyle.title}>Welcome, {name}</Text>
     </View>
   );
 }
