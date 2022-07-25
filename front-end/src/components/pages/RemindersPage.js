1937653fca89196e83fbdcd442c3afcb4cde141e
AliOlliek <olliek95@gmail.com>
2022-07-25 16:31:10 +0300

    FE - Added Function to Get Reminders

--- front-end/src/components/pages/RemindersPage.js
+++ front-end/src/components/pages/RemindersPage.js
@@ -1,4 +1,8 @@
-// React
+// Modules
+import axios from "axios";
+import { useSelector } from "react-redux";
+import { useCallback, useState } from "react";
+import { useFocusEffect } from "@react-navigation/native";
 import { View, Text, TouchableHighlight } from "react-native";
 
 // Styles
@@ -10,6 +14,39 @@ import { MainHeaderTitle } from "../UI/atoms";
 import { TodosList } from "../UI/molecules/TodosList";
 
 export function RemindersPage({ navigation }) {
+  //
+  // States and variables
+  const [remindersList, setRemindersList] = useState([]);
+  const { id, token } = useSelector((state) => state.user.user);
+
+  //
+  // Create Request
+  const getRemindersUrl = `http://10.0.2.2:8000/api/v1/farmReminders/?user_id=${id}`;
+  const data = { user_id: id };
+  const getReminders = () => {
+    axios({
+      method: "GET",
+      url: getRemindersUrl,
+      data: data,
+      headers: { Authorization: token },
+    })
+      .then((response) => {
+        if (response.data.code === 200) {
+          setRemindersList(response.data.reminders);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  //
+  // Call on Navigation
+  useFocusEffect(
+    useCallback(() => {
+      getReminders();
+    }, [])
+  );
+
   return (
     <>
       <View style={styles.header}>
@@ -31,7 +68,13 @@ export function RemindersPage({ navigation }) {
           <Text>Create Reminder</Text>
         </TouchableHighlight>
       </View>
-      <TodosList />
+      {remindersList.length !== 0 ? (
+        <TodosList todos={remindersList} />
+      ) : (
+        <View style={Todos.empty}>
+          <Text>No Reminders to Display</Text>
+        </View>
+      )}
     </>
   );
 }
