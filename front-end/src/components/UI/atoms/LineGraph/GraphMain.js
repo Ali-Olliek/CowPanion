4ef430109135ce421a81f08019be6487db60016d
AliOlliek <olliek95@gmail.com>
2022-07-26 15:59:04 +0300

    FE - Added Conditional Rendering if Props are Present

--- front-end/src/components/UI/atoms/LineGraph/GraphMain.js
+++ front-end/src/components/UI/atoms/LineGraph/GraphMain.js
@@ -1,26 +1,14 @@
-import React, { Component } from "react";
-import { StyleSheet, View } from "react-native";
+// Modules
+import React from "react";
+import { Dimensions, StyleSheet, View } from "react-native";
 
-import { BarChart } from "./LineGraph";
+// Components
+import { BarChart } from "./BarGraph";
 
-export function GraphMain() {
-  const data = [
-    { label: "Jan", value: 200 },
-    { label: "Feb", value: 312 },
-    { label: "Mar", value: 424 },
-    { label: "Apr", value: 745 },
-    { label: "May", value: 89 },
-    { label: "Jun", value: 434 },
-    { label: "Jul", value: 650 },
-    { label: "Aug", value: 980 },
-    { label: "Sep", value: 123 },
-    { label: "Oct", value: 186 },
-    { label: "Nov", value: 689 },
-    { label: "Dec", value: 643 },
-  ];
+export function GraphMain({ milkData }) {
   return (
     <View style={styles.container}>
-      <BarChart data={data} />
+      {milkData && <BarChart milkData={milkData} />}
     </View>
   );
 }
@@ -30,6 +18,5 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    marginTop: 100,
   },
 });
