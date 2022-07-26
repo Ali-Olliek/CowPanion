80cdb3af14bdec8389d53f37a2131de56a49234e
AliOlliek <olliek95@gmail.com>
2022-07-26 18:03:09 +0300

    FE - Readjusted Navigation for Farmer

--- front-end/App.js
+++ front-end/App.js
@@ -1,10 +1,8 @@
-import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { Provider } from "react-redux";
 import { OnBoardingPage } from "./src/components/pages/OnBoardingPage";
-import { configureStore } from "@reduxjs/toolkit";
-
+import { useSelector, Provider } from "react-redux";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import {
   AnimalInformationPage,
   AnimalsListPage,
@@ -16,45 +14,62 @@ import {
   RemindersPage,
   UpdateStatusPage,
   ProfilePage,
+  MainPage,
+  AddAnimalPage,
+  CreateFarmPage,
 } from "./src/components/pages";
 
 import { store } from "./src/redux/store";
+import { useState } from "react";
 
 export default function App() {
   const Stack = createNativeStackNavigator();
+  // Fetch Data From Local Storage
+  const [userData, setUserData] = useState("");
+  // const getUserData = async () => {
+  //   data = await AsyncStorage.getItem("userData");
+  //   setUserData(data);
+  // };
+  // getUserData();
 
+  // const { isLogged } = useSelector((state) => console.log(state));
+  const RootNav = () => {
+    return (
+      <Provider store={store}>
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="SignIn" component={LoginPage} />
+            <Stack.Screen name="SignUp" component={SignUpPage} />
+            <Stack.Screen name="LandingPage" component={LandingPage} />
+            <Stack.Screen
+              name="createReminder"
+              component={CreateReminderPage}
+            />
+            <Stack.Screen name="Animals" component={AnimalsListPage} />
+            <Stack.Screen name="Animal" component={AnimalInformationPage} />
+            <Stack.Screen name="Reminders" component={RemindersPage} />
+            <Stack.Screen name="OnBoarding" component={OnBoardingPage} />
+            <Stack.Screen name="AddAnimal" component={AddAnimalPage} />
+            <Stack.Screen
+              name="New Medical Record"
+              component={CreateMedicalRecordPage}
+            />
+            <Stack.Screen
+              name="userProfile"
+              props={setUserData}
+              component={ProfilePage}
+            />
+            <Stack.Screen name="Update Status" component={UpdateStatusPage} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </Provider>
+    );
+  };
+
+  // MAIN
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="SignIn"
-          screenOptions={{ headerShown: false }}
-        >
-          <Stack.Screen name="SignIn" component={LoginPage} />
-          <Stack.Screen name="SignUp" component={SignUpPage} />
-          <Stack.Screen name="LandingPage" component={LandingPage} />
-          <Stack.Screen name="createReminder" component={CreateReminderPage} />
-          <Stack.Screen name="Animals" component={AnimalsListPage} />
-          <Stack.Screen name="Animal" component={AnimalInformationPage} />
-          <Stack.Screen name="Reminders" component={RemindersPage} />
-          <Stack.Screen name="OnBoarding" component={OnBoardingPage} />
-          <Stack.Screen
-            name="New Medical Record"
-            component={CreateMedicalRecordPage}
-          />
-          <Stack.Screen name="userProfile" component={ProfilePage} />
-          <Stack.Screen name="Update Status" component={UpdateStatusPage} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <RootNav />
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "white",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
