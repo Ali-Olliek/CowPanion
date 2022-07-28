cfdb831d7150ea357367590a19efa87e11b987b0
AliOlliek <olliek95@gmail.com>
2022-07-28 07:18:29 +0300

    FE - Improved Styling

--- front-end/src/styles/createFarmStyle.js
+++ front-end/src/styles/createFarmStyle.js
@@ -7,15 +7,13 @@ const createFarmStyle = StyleSheet.create({
   midSec: {
     justifyContent: "space-evenly",
     alignItems: "center",
-    height: 0.8 * windowHeight,
   },
   inputs: {
-    backgroundColor: "#DAD7CD",
-    height: 60,
-    width: 0.6 * windowWidth,
-    alignItems: "center",
-    textAlign: "center",
-    borderRadius: 8,
+    height: 40,
+    width: 0.9 * windowWidth,
+    borderBottomWidth: 1,
+    textAlign: "left",
+    paddingLeft: 10,
   },
   GO: {
     borderWidth: 1,
@@ -24,13 +22,43 @@ const createFarmStyle = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     borderRadius: 10,
-    marginTop: 100,
+    marginVertical: 20,
     backgroundColor: "#ACC0B3",
   },
   inputsContainer: {
-    height: 0.2 * windowHeight,
+    height: 0.8 * windowHeight,
     justifyContent: "space-between",
-    marginVertical: 20,
+    paddingHorizontal: 20,
+    marginTop: 10,
+  },
+  placeholder: {
+    fontSize: 10,
+    borderBottomWidth: 1,
+    width: 0.9 * windowWidth,
+    height: 40,
+    paddingLeft: 10,
+  },
+  vetList: {
+    marginVertical: 5,
+    height: 50,
+  },
+  vet: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    height: 30,
+  },
+  listContainer: {
+    marginVertical: 5,
+  },
+  active: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    height: 30,
+    backgroundColor: "red",
+  },
+  data: {
+    height: 0.25 * windowHeight,
+    justifyContent: "space-between",
   },
 });
 
