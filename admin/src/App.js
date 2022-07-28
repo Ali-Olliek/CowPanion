eddcb504e8358c60623756cdf9caee04938b4113
AliOlliek <olliek95@gmail.com>
2022-07-28 16:16:13 +0300

    Admin - Defined Route in Appjs

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,8 +1,14 @@
+import LoginPage from "./pages/Login/LoginPage";
+import MainPage from "./pages/Main/MainPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />}></Route>
+        <Route path="/Main" element={<MainPage />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
