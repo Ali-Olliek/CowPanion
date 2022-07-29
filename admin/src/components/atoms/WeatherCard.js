1561ab7a48a1ce270322e3a916ad149705401c95
AliOlliek <olliek95@gmail.com>
2022-07-29 22:52:46 +0300

    Admin - Restyled Top Bar

--- admin/src/components/atoms/WeatherCard.js
+++ admin/src/components/atoms/WeatherCard.js
@@ -3,9 +3,8 @@ import "../../pages/Main/TopBar/TopBarStyles.css";
 export default function WeatherCard({ reading }) {
   return (
     <div className="weatherCard">
-      <h1>{reading[1].value}</h1>
-      <h6>{reading[1].unit}</h6>
-      <h3>icon</h3>
+      <h1 className="weatherValue">{reading[1].value}</h1>
+      <h6 className="unit">{reading[1].unit}</h6>
     </div>
   );
 }
