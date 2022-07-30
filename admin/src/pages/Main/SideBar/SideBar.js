ecdd5b8a55937fd8be59adcffcac9f4d1da6d9bb
AliOlliek <olliek95@gmail.com>
2022-07-30 11:51:02 +0300

    Admin - Added Logout

--- admin/src/pages/Main/SideBar/SideBar.js
+++ admin/src/pages/Main/SideBar/SideBar.js
@@ -1,15 +1,22 @@
 import React from "react";
 import "./SideBarStyles.css";
-
+import { useNavigate } from "react-router-dom";
 export default function SideBar() {
+  const navigate = useNavigate();
   const adminName = localStorage.getItem("name");
+  const handleLogOut = () => {
+    localStorage.removeItem("token");
+    navigate("/");
+  };
   return (
     <div className="sideBar">
       <div className="adminBox">
         <h4>{adminName}</h4>
       </div>
       <div>
-        <button className="button">Log out</button>
+        <button onClick={handleLogOut} className="button">
+          Log out
+        </button>
       </div>
     </div>
   );
