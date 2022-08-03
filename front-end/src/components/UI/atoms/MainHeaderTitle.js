d50a562413a8c5dea80232f00a68cc865ad26992
AliOlliek <olliek95@gmail.com>
2022-08-03 21:42:33 +0300

    FE - Removed Add Animal Button For Veterinarians

--- front-end/src/components/UI/atoms/MainHeaderTitle.js
+++ front-end/src/components/UI/atoms/MainHeaderTitle.js
@@ -1,19 +1,22 @@
 import { View, Text, TouchableHighlight } from "react-native";
 import { styles } from "../../../styles/AnimalsListStyle";
-
+import { useSelector } from "react-redux";
 export function MainHeaderTitle({ title, subtitle, navigation, nav }) {
+  const { userType } = useSelector((state) => state.user.user);
   return (
     <View style={{ flexDirection: "column", paddingTop: 20 }}>
       <Text style={styles.subTitle}>{subtitle}</Text>
       <View style={styles.bottomTitle}>
         <Text style={styles.titles}>{title}</Text>
-        <TouchableHighlight
-          underlayColor={"#307A55"}
-          onPress={() => navigation.navigate(nav)}
-          style={styles.AddAnimalButton}
-        >
-          <Text style={styles.plus}>+</Text>
-        </TouchableHighlight>
+        {userType == 2 ? (
+          <TouchableHighlight
+            underlayColor={"#307A55"}
+            onPress={() => navigation.navigate(nav)}
+            style={styles.AddAnimalButton}
+          >
+            <Text style={styles.plus}>+</Text>
+          </TouchableHighlight>
+        ) : null}
       </View>
     </View>
   );
