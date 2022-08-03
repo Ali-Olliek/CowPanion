1906bb758c4a7b2d8f1b53389f71205732afd60d
AliOlliek <olliek95@gmail.com>
2022-08-03 20:51:10 +0300

    Admin - Added OpenWeatherAPI to Weather Cards

--- admin/src/components/atoms/WeatherCard.js
+++ admin/src/components/atoms/WeatherCard.js
@@ -1,10 +1,21 @@
 import React from "react";
 import "../../pages/Main/TopBar/TopBarStyles.css";
 export default function WeatherCard({ reading }) {
+  console.log(reading);
   return (
-    <div className="weatherCard">
-      <h1 className="weatherValue">{reading[1].value}</h1>
-      <h6 className="unit">{reading[1].unit}</h6>
-    </div>
+    <>
+      <div className="weatherCard">
+        <h1 className="weatherValue">{Math.round(reading.temp)}</h1>
+        <h6 className="unit">°C</h6>
+      </div>
+      <div className="weatherCard">
+        <h1 className="weatherValue">{Math.round(reading.humidity)}</h1>
+        <h6 className="unit">%</h6>
+      </div>
+      <div className="weatherCard">
+        <h1 className="weatherValue">{Math.round(reading.wind)}</h1>
+        <h6 className="unit">Km/hr</h6>
+      </div>
+    </>
   );
 }
--- admin/src/components/molecules/WeatherCards.js
+++ admin/src/components/molecules/WeatherCards.js
@@ -1,21 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import WeatherCard from "../atoms/WeatherCard";
 import "../../pages/Main/TopBar/TopBarStyles.css";
-
+import axios from "axios";
 export default function WeatherCards() {
-  const weatherData = {
-    Temperature: { value: 30, unit: "Celsius" },
-    humidity: { value: 60, unit: "%" },
-    windSpeed: {
-      value: 40,
-      unit: "Km/Hr",
-    },
+  const [weatherData, setWeatherData] = useState({
+    temp: "",
+    humidity: "",
+    wind: "",
+  });
+
+  const getWeather = () => {
+    axios({
+      url: "https://api.openweathermap.org/data/2.5/weather?q=Beirut&units=metric&appid=9f706b68bb6d46c94afc8cb995634417",
+      method: "GET",
+    })
+      .then((response) => {
+        setWeatherData({
+          temp: response.data.main.temp,
+          humidity: response.data.main.humidity,
+          wind: response.data.wind.speed,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
+
+  useEffect(() => {
+    getWeather();
+  }, []);
+
   return (
     <div className="weatherCards">
-      {Object.entries(weatherData).map((reading, index) => {
-        return <WeatherCard reading={reading} />;
-      })}
+      <WeatherCard reading={weatherData} />
     </div>
   );
 }
