184e16df26ce29ba4bb0ea442a088ac146b64482
AliOlliek <olliek95@gmail.com>
2022-08-04 22:04:44 +0300

    FE - Created BottomNavBar for Vets

--- front-end/src/components/UI/organisms/BottomNavBarButtons.js
+++ front-end/src/components/UI/organisms/BottomNavBarButtons.js
@@ -11,67 +11,128 @@ import {
   MainPage,
   CameraPage,
 } from "../../pages";
-
+import { useSelector } from "react-redux";
 export function BottomNavBarButtons() {
   const Tab = createMaterialBottomTabNavigator();
+  const { id, userType } = useSelector((state) => state.user.user);
+
+  const farmerBottomNav = () => {
+    return (
+      <Tab.Navigator
+        screenOptions={{ headerShown: false }}
+        initialRouteName="Home"
+        activeColor="#F17754"
+        inactiveColor="#fff"
+        barStyle={{ backgroundColor: "#307A55" }}
+      >
+        <Tab.Screen
+          options={{
+            tabBarLabel: "",
+            tabBarIcon: ({ color }) => (
+              <Feather name="home" size={24} color={color} />
+            ),
+          }}
+          name={"Home"}
+          component={MainPage}
+        />
+        <Tab.Screen
+          options={{
+            tabBarLabel: "",
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="cow" size={26} color={color} />
+            ),
+          }}
+          name={"AnimalsList"}
+          component={AnimalsListPage}
+        />
+        <Tab.Screen
+          options={{
+            tabBarLabel: "",
+            tabBarIcon: ({ color }) => (
+              <Feather name="camera" size={24} color={color} />
+            ),
+          }}
+          name={"Camera"}
+          component={CameraPage}
+        />
+        <Tab.Screen
+          options={{
+            tabBarLabel: "",
+            tabBarIcon: ({ color }) => (
+              <Feather name="list" size={24} color={color} />
+            ),
+          }}
+          name={"Reminders"}
+          component={RemindersPage}
+        />
+        <Tab.Screen
+          options={{
+            tabBarLabel: "",
+            tabBarIcon: ({ color }) => (
+              <Ionicons name="person-outline" size={24} color={color} />
+            ),
+          }}
+          name={"Profile"}
+          component={ProfilePage}
+        />
+      </Tab.Navigator>
+    );
+  };
+
+  const vetBottomNav = () => {
+    return (
+      <Tab.Navigator
+        screenOptions={{ headerShown: false }}
+        initialRouteName="AnimalsList"
+        activeColor="#F17754"
+        inactiveColor="#fff"
+        barStyle={{ backgroundColor: "#307A55" }}
+      >
+        <Tab.Screen
+          options={{
+            tabBarLabel: "",
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="cow" size={26} color={color} />
+            ),
+          }}
+          name={"AnimalsList"}
+          component={AnimalsListPage}
+        />
+        <Tab.Screen
+          options={{
+            tabBarLabel: "",
+            tabBarIcon: ({ color }) => (
+              <Feather name="camera" size={24} color={color} />
+            ),
+          }}
+          name={"Camera"}
+          component={CameraPage}
+        />
+        <Tab.Screen
+          options={{
+            tabBarLabel: "",
+            tabBarIcon: ({ color }) => (
+              <Feather name="list" size={24} color={color} />
+            ),
+          }}
+          name={"Reminders"}
+          component={RemindersPage}
+        />
+        <Tab.Screen
+          options={{
+            tabBarLabel: "",
+            tabBarIcon: ({ color }) => (
+              <Ionicons name="person-outline" size={24} color={color} />
+            ),
+          }}
+          name={"Profile"}
+          component={ProfilePage}
+        />
+      </Tab.Navigator>
+    );
+  };
+
   return (
-    <Tab.Navigator
-      screenOptions={{ headerShown: false }}
-      initialRouteName="Home"
-      activeColor="#F17754"
-      inactiveColor="#fff"
-      barStyle={{ backgroundColor: "#307A55" }}
-    >
-      <Tab.Screen
-        options={{
-          tabBarLabel: "",
-          tabBarIcon: ({ color }) => (
-            <Feather name="home" size={24} color={color} />
-          ),
-        }}
-        name={"Home"}
-        component={MainPage}
-      />
-      <Tab.Screen
-        options={{
-          tabBarLabel: "",
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="cow" size={26} color={color} />
-          ),
-        }}
-        name={"AnimalsList"}
-        component={AnimalsListPage}
-      />
-      <Tab.Screen
-        options={{
-          tabBarLabel: "",
-          tabBarIcon: ({ color }) => (
-            <Feather name="camera" size={24} color={color} />
-          ),
-        }}
-        name={"Camera"}
-        component={CameraPage}
-      />
-      <Tab.Screen
-        options={{
-          tabBarLabel: "",
-          tabBarIcon: ({ color }) => (
-            <Feather name="list" size={24} color={color} />
-          ),
-        }}
-        name={"Reminders"}
-        component={RemindersPage}
-      />
-      <Tab.Screen
-        options={{
-          tabBarLabel: "",
-          tabBarIcon: ({ color }) => (
-            <Ionicons name="person-outline" size={24} color={color} />
-          ),
-        }}
-        name={"Profile"}
-        component={ProfilePage}
-      />
-    </Tab.Navigator>
+    <>{userType && <>{userType == 2 ? farmerBottomNav() : vetBottomNav()}</>}</>
   );
 }
