5ea659601fe50df97a717fbc43cc0ebfe2cb197c
AliOlliek <olliek95@gmail.com>
2022-08-04 22:07:27 +0300

    FE - Added OpenWeatherAPI

--- front-end/src/components/UI/atoms/WeatherCard.js
+++ front-end/src/components/UI/atoms/WeatherCard.js
@@ -1,14 +1,39 @@
 import { View, Text, Image } from "react-native";
 import { headerSectionStyle } from "../../../styles/headerSectionStyle";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 // icons
 import { FontAwesome } from "@expo/vector-icons";
 import { Feather } from "@expo/vector-icons";
 import { Ionicons } from "@expo/vector-icons";
 export function WeatherCard() {
-  const weatherData = require("../../../assets/files/weatherJSON.json");
-  const weather_url =
-    "https://api.openweathermap.org/data/2.5/weather?q=Beirut&units=metric&appid=9f706b68bb6d46c94afc8cb995634417";
+  const [weatherData, setWeatherData] = useState({
+    temp: "",
+    humidity: "",
+    wind: "",
+  });
+
+  const getWeather = () => {
+    axios({
+      url: "https://api.openweathermap.org/data/2.5/weather?q=Beirut&units=metric&appid=9f706b68bb6d46c94afc8cb995634417",
+      method: "GET",
+    })
+      .then((response) => {
+        setWeatherData({
+          temp: response.data.main.temp,
+          humidity: response.data.main.humidity,
+          wind: response.data.wind.speed,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    getWeather();
+  }, []);
 
   return (
     <View style={headerSectionStyle.weatherCardsContainer}>
@@ -26,19 +51,19 @@ export function WeatherCard() {
         <View style={headerSectionStyle.weatherCard}>
           <FontAwesome name="thermometer-1" size={35} color="#D46C4E" />
           <Text style={headerSectionStyle.weatherReading}>
-            {weatherData.main.temp} °C
+            {weatherData.temp} °C
           </Text>
         </View>
         <View style={headerSectionStyle.weatherCard}>
           <Ionicons name="water-outline" size={35} color="#D46C4E" />
           <Text style={headerSectionStyle.weatherReading}>
-            {weatherData.main.humidity} %
+            {weatherData.humidity} %
           </Text>
         </View>
         <View style={headerSectionStyle.weatherCard}>
           <Feather name="wind" size={35} color="#D46C4E" />
           <Text style={headerSectionStyle.weatherReading}>
-            {weatherData.wind.speed} <Text style={{ fontSize: 12 }}>Km/Hr</Text>
+            {weatherData.wind} <Text style={{ fontSize: 12 }}>Km/Hr</Text>
           </Text>
         </View>
       </View>
